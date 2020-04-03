4fa6700014b32b7e8acbf4dde2a38104970620e2
kur0ikenshi <63129864+kur0ikenshi@users.noreply.github.com>
2020-04-03 20:54:16 -0300

    Add files via upload

---
+++ learning/src/learning/MainClass.java
@@ -0,0 +1,12 @@
+package learning;
+
+public class MainClass 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("manga com sal");
+	}
+
+}
